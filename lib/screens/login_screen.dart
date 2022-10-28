71032baf57b0416705e2f21eaf84230628d2a02c
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-10-28 23:05:18 +0300

    can use public methods

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:nft_domains_dapp/contracts/Domains.g.dart';
+import 'package:web3dart/web3dart.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,6 +12,24 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<void> _getAddress() async {
+    const apiUrl = "https://matic-mumbai.chainstacklabs.com";
+    final address =
+        EthereumAddress.fromHex("0x9Bd1110435cA360E6e999ba5b46B87421aD3D4b7");
+
+    var httpClient = Client();
+    var ethClient = Web3Client(apiUrl, httpClient);
+    final stub = Domains(address: address, client: ethClient);
+    final result = await stub.getOwner('ahmets');
+    print(result);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -18,6 +39,7 @@ class _LoginScreenState extends State<LoginScreen> {
           child: ElevatedButton(
         child: const Text("Connect With Metamask"),
         onPressed: () async {
+          await _getAddress();
           // await qrCodeModal.connect(context, chainId: 80001);
           // loginUsingMetamask(context);
         },
