a886a0835990e86c88cdf9d8a20dc6fbe96baa33
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-10-29 14:57:43 +0300

    cleaning

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:nft_domains_dapp/app/home/view/home_page.dart';
+import 'package:nft_domains_dapp/app/home/home.dart';
 import 'package:nft_domains_dapp/features/wallet/wallet_repository.dart';
 import 'package:provider/provider.dart';
-import 'authentication/auth_info.dart';
+import 'authentication/authentication.dart';
 import 'login/login.dart';
 
 class App extends StatelessWidget {
