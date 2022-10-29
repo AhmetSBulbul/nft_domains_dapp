143f67a8cb6e144db15a3add3fd25f02f1e81f40
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-10-29 15:53:58 +0300

    contract folder moved to domain_service

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:nft_domains_dapp/contracts/Domains.g.dart';
+import 'package:nft_domains_dapp/features/domain_service/contract/Domains.g.dart';
+
 import 'package:nft_domains_dapp/features/wallet/entity/wallet_connect_ethereum_credentials.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
